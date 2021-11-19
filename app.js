128f5ca7c1ddc873c7e2f7a184c1cf292070161d
julian776 <juliancamiloalvarez77@gmail.com>
2021-11-19 11:46:54 -0500

    Principal classes created and working(Board, createCanvas)

---
+++ app.js
@@ -0,0 +1,13 @@
+(function(){
+    let canv = document.getElementById("canvas")
+    let canvas = canv.getContext("2d")
+    
+    class Board{
+        constructor(heigth, width){
+            this.heigth = heigth
+            this.width = width
+            this.playing = false
+            this.game_over = false
+        }
+    }
+}())
